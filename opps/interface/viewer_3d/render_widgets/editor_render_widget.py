531f13c980dd1724e4917c0bfd0f4b5d95b64238
André Fernandes <fpf.andre@gmail.com>
2023-09-24 14:51:35 -0300

    add tmp tube example

--- opps/interface/viewer_3d/render_widgets/editor_render_widget.py
+++ opps/interface/viewer_3d/render_widgets/editor_render_widget.py
@@ -13,9 +13,24 @@ class EditorRenderWidget(CommonRenderWidget):
         super().__init__(parent)
 
         self.pipeline = Pipeline()
-        self.pipeline.add_pipe((0, 0, 0), (0, 1, 0))
-        self.pipeline.add_pipe((1, 2, 0), (2, 2, 0))
-        self.pipeline.add_bend((0, 1, 0), (1, 2, 0), (1, 1, 0))
+        self.pipeline.add_pipe_from_deltas(
+            (236, 0, 0),
+            (118, 0, 0),
+            (123, 0, -123),
+            (0, 380, 0),
+            (0, 0, 307),
+            (0, 0, 801),
+        )
+        # self.pipeline.add_flange((0,0,0), (0,1,0))
+        # self.pipeline.add_pipe_from_points(
+        #     (0,0,0),
+        #     (1,0,0),
+        #     (2,1,0),
+        #     (2,1,-1),
+        # )
+        # self.pipeline.add_pipe((0, 0, 0), (0, 1, 0))
+        # self.pipeline.add_pipe((1, 2, 0), (2, 2, 0))
+        # self.pipeline.add_bend((0, 1, 0), (1, 2, 0), (1, 1, 0))
 
         self.fixed_point_actor = None
         self.pipeline_actor = None
@@ -31,6 +46,7 @@ class EditorRenderWidget(CommonRenderWidget):
         self.renderer.AddActor(self.fixed_point_actor)
         self.renderer.AddActor(self.pipeline_actor)
         self.renderer.ResetCamera()
+        self.set_isometric_view()
 
     def remove_actors(self):
         self.renderer.RemoveActor(self.fixed_point_actor)
