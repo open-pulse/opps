03d901b5efeea49e3766dc507d8cbe67eb36f20e
vitorslongo <v.v.slongo@gmail.com>
2023-11-03 18:32:30 -0300

    working lines creator

--- opps/io/cad_file/step_exporter.py
+++ opps/io/cad_file/step_exporter.py
@@ -1,4 +1,7 @@
 import gmsh
+from opps.model.pipe import Pipe
+from opps.model.bend import Bend
+from opps.model.flange import Flange
 
 
 class StepExporter:
@@ -8,14 +11,43 @@ class StepExporter:
     def save(self, path, pipeline):
         # print(path)
         # print(pipeline.components)
-        
-        for component in pipeline.components:   
-            print(type(component))
+        lines_guide = []
+        gmsh.initialize("", False)
+        for component in pipeline.components: 
+            if type(component) == Pipe:
+                start = (tuple(component.start)[0], tuple(component.start)[1], tuple(component.start)[2])
+                end = (tuple(component.end)[0], tuple(component.end)[1], tuple(component.end)[2])
 
-    x0 = 0
-    y0 = 0 # these need to be the coordinates of the last x, y, z plus the dx, dy, dz
-    z0 = 0
-    gmsh.model.occ.add_cylinder(x0, y0, z0, dx, dy, dz, r, -1) # it adds a cap to the cylinder, need to solve this  
-    # gmsh.model.occ.add_torus()? #for the bends; Maybe there is a better way to do this, perhaps using a curved line and sweeping a circle on it
-    # add_flange needs to be modeled in some CAD 
-    
+                start_point = gmsh.model.occ.add_point(start[0], start[1], start[2])
+                end_point = gmsh.model.occ.add_point(end[0], end[1], end[2])
+
+                gmsh.model.occ.add_line(start_point, end_point)
+
+            # elif type(component) == Bend:
+            #     print(component)
+            #     start = (tuple(component.start)[0], tuple(component.start)[1], tuple(component.start)[2])
+            #     end = (tuple(component.end)[0], tuple(component.end)[1], tuple(component.end)[2])
+            #     center = (tuple(component.center)[0], tuple(component.center)[1], tuple(component.center)[2])
+
+                
+            #     gmsh.model.occ.add_circle_arc()
+
+
+               
+        # last_point = gmsh.model.occ.add_point(tuple(component.end)[0], tuple(component.end)[1], tuple(component.end)[2])
+        # lines_guide.append(last_point)
+
+        # for i in range(len(lines_guide) - 1):
+        #     gmsh.model.occ.add_line(lines_guide[i], lines_guide[i+1])
+                
+
+
+        
+        gmsh.model.occ.synchronize()
+        gmsh.fltk.run()
+
+
+
+
+
+
