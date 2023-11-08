f9a4bdcc5f6566f5da56bea47c936fdc38de3f21
vitorslongo <v.v.slongo@gmail.com>
2023-11-08 14:50:27 -0300

    improved the exporter code

--- opps/io/cad_file/step_exporter.py
+++ opps/io/cad_file/step_exporter.py
@@ -9,43 +9,25 @@ class StepExporter:
         pass
 
     def save(self, path, pipeline):
-
         gmsh.initialize("", False)
         for component in pipeline.components: 
 
-            if type(component) == Pipe:
-                start = (tuple(component.start)[0], tuple(component.start)[1], tuple(component.start)[2])
-                end = (tuple(component.end)[0], tuple(component.end)[1], tuple(component.end)[2])
-
-                start_point = gmsh.model.occ.add_point(start[0], start[1], start[2])
-                end_point = gmsh.model.occ.add_point(end[0], end[1], end[2])
+            if isinstance(component, Pipe):
+                start_point = gmsh.model.occ.add_point(*component.start)
+                end_point = gmsh.model.occ.add_point(*component.end)
 
                 gmsh.model.occ.add_line(start_point, end_point)
 
-            elif type(component) == Bend:
-                print(component)
-                start = (tuple(component.start)[0], tuple(component.start)[1], tuple(component.start)[2])
-                end = (tuple(component.end)[0], tuple(component.end)[1], tuple(component.end)[2])
-                center = (tuple(component.center)[0], tuple(component.center)[1], tuple(component.center)[2])
-
-                start_point = gmsh.model.occ.add_point(start[0], start[1], start[2])
-                end_point = gmsh.model.occ.add_point(end[0], end[1], end[2])
-                center_point = gmsh.model.occ.add_point(center[0], center[1], center[2])
+            elif isinstance(component, Bend):
+                start_point = gmsh.model.occ.add_point(*component.start)
+                end_point = gmsh.model.occ.add_point(*component.end)
+                center_point = gmsh.model.occ.add_point(*component.center)
 
                 gmsh.model.occ.add_circle_arc(start_point, center_point, end_point)
 
-            # elif type(component) == Flange:
-            #     print(component)
-            #     position = (tuple(component.position)[0], tuple(component.position)[1], tuple(component.position)[2])
-            #     position_point = gmsh.model.occ.add_point(position[0], position[1], position[2])
-            #     gmsh.model.occ.add_circle()
-
-            # there's still the flange left to do, the idea is to create a circle to denote it
-            # duplicate points are being created on the end and start of pipes (lines)
-
         gmsh.model.occ.synchronize()
-        gmsh.write("YourFile.step")
-        # gmsh.fltk.run()
+        gmsh.write(path)
+        gmsh.fltk.run()
 
 
 
