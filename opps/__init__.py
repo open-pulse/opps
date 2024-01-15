2f5c9635e196e3d826c4fc17983e1f5912f79d37
André Fernandes <fpf.andre@gmail.com>
2024-01-15 17:17:35 -0300

    move ui_files inside the project so poetry can create a package with it

--- opps/__init__.py
+++ opps/__init__.py
@@ -1,5 +1,10 @@
 from PyQt5.QtWidgets import QApplication
+from pathlib import Path
 
+
+ROOT_DIR = Path(__file__).parent
+UI_DIR = ROOT_DIR / "interface/ui_files"
 
 def app() -> "Application":
     return QApplication.instance()
+
