baa97da66e654f408df6a294c964906ee04cc336
André Fernandes <fpf.andre@gmail.com>
2024-02-06 17:20:05 -0300

    add a tag for every entity before saving cad

--- opps/io/cad_file/cad_handler.py
+++ opps/io/cad_file/cad_handler.py
@@ -6,26 +6,27 @@ from opps.model.pipe import Pipe
 
 
 class CADHandler:
-    def __init__(self):
-        pass
-
     def save(self, path, pipeline):
         gmsh.initialize("", False)
-        for structure in pipeline.structures:
+        for i, structure in enumerate(pipeline.structures, 1):
+            # The tag of the structures isn't really a thing after the
+            # file is saved, but at least it preserves the desired ordering.
+
             if isinstance(structure, Pipe):
                 start_point = gmsh.model.occ.add_point(*structure.start.coords())
                 end_point = gmsh.model.occ.add_point(*structure.end.coords())
 
-                gmsh.model.occ.add_line(start_point, end_point)
+                gmsh.model.occ.add_line(start_point, end_point, tag=i)
 
             elif isinstance(structure, Bend):
                 if structure.is_colapsed():
                     continue
+
                 start_point = gmsh.model.occ.add_point(*structure.start.coords())
                 end_point = gmsh.model.occ.add_point(*structure.end.coords())
                 center_point = gmsh.model.occ.add_point(*structure.center.coords())
 
-                gmsh.model.occ.add_circle_arc(start_point, center_point, end_point)
+                gmsh.model.occ.add_circle_arc(start_point, center_point, end_point, tag=i)
 
         gmsh.model.occ.synchronize()
         gmsh.write(str(path))
