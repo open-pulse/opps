9aecffbdf11ddcfbcb15a24c47302848c769f125
André Fernandes <fpf.andre@gmail.com>
2024-02-28 18:03:41 -0300

    implement rectangular beam rotations

--- opps/interface/viewer_3d/actors/beam_actors/rectangular_beam_actor.py
+++ opps/interface/viewer_3d/actors/beam_actors/rectangular_beam_actor.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from opps.model import RectangularBeam
 from opps.interface.viewer_3d.utils.cross_section_sources import rectangular_beam_data
+from opps.interface.viewer_3d.utils.rotations import align_y_rotations
 
 
 class RectangularBeamActor(vtk.vtkActor):
@@ -11,10 +12,16 @@ class RectangularBeamActor(vtk.vtkActor):
         self.create_geometry()
 
     def create_geometry(self):
-        length = np.linalg.norm(self.beam.end.coords() - self.beam.start.coords())
+        vector = self.beam.end.coords() - self.beam.start.coords()
+        length = np.linalg.norm(vector)
         source = rectangular_beam_data(length, self.beam.width, self.beam.height, self.beam.thickness)
 
+        rx, ry, rz = align_y_rotations(vector)
         transform = vtk.vtkTransform()
+        transform.RotateZ(-np.degrees(rz))
+        transform.RotateY(-np.degrees(ry))
+        transform.RotateX(-np.degrees(rx))
+        transform.Translate(0, length/2, 0)
         transform.Update()
 
         transform_filter = vtk.vtkTransformFilter()
---
+++ opps/interface/viewer_3d/utils/rotations.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def align_y_rotations(vector):
+    x, y, z = vector
+    rx, ry, rz = 0, 0, 0
+
+    xy_length = np.sqrt(x * x + y * y)
+    vector_length = np.sqrt(x*x + y*y + z*z)
+    
+    if xy_length:
+        rz = np.arccos(y / xy_length)
+
+    if vector_length:
+        rx = -np.arccos(xy_length / vector_length)
+
+    return rx, ry, rz
