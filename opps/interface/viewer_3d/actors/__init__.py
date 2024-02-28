52b6ca05f5381812ca8e3dc38a7cb9b09a0a5648
André Fernandes <fpf.andre@gmail.com>
2024-02-28 13:10:56 -0300

    move all point actors to a common dir

--- opps/interface/viewer_3d/actors/__init__.py
+++ opps/interface/viewer_3d/actors/__init__.py
@@ -1,3 +1,3 @@
-from .control_points_actor import *
-from .passive_points_actor import *
-from .selected_points_actor import *
+from .point_actors.control_points_actor import *
+from .point_actors.passive_points_actor import *
+from .point_actors.selected_points_actor import *
