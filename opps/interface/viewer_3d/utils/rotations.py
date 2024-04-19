e13b1b014b6082f90bcafd905f22d1b6b1b71cc3
André Fernandes <fpf.andre@gmail.com>
2024-04-19 17:00:12 -0300

    fix structure rotations

--- opps/interface/viewer_3d/utils/rotations.py
+++ opps/interface/viewer_3d/utils/rotations.py
@@ -3,23 +3,29 @@ import vtk
 
 
 def align_y_rotations(vector):
+    "https://www.fundza.com/mel/axis_to_vector/index.html"
+
     x, y, z = vector
     rx, ry, rz = 0, 0, 0
 
     xy_length = np.sqrt(x * x + y * y)
     vector_length = np.sqrt(x * x + y * y + z * z)
 
-    if x < 0:
-        xy_length = -xy_length
+    if vector_length == 0:
+        return 0, 0, 0
 
     if xy_length:
         rz = np.arccos(y / xy_length)
+        ry = np.pi
+    else:
+        rz = np.pi / 2
 
-    if vector_length:
-        rx = -np.arccos(xy_length / vector_length)
-
+    rx = np.arccos(xy_length / vector_length)
     if z < 0:
         rx = -rx
+
+    if x > 0:
+        rz = -rz
 
     return rx, ry, rz
 
@@ -30,9 +36,9 @@ def align_vtk_geometry(geometry: vtk.vtkPolyData, start: np.ndarray, vector: np.
 
     transform = vtk.vtkTransform()
     transform.Translate(x, y, z)
-    transform.RotateZ(-np.degrees(rz))
-    transform.RotateY(-np.degrees(ry))
-    transform.RotateX(-np.degrees(rx))
+    transform.RotateZ(np.degrees(rz))
+    transform.RotateX(np.degrees(rx))
+    transform.RotateY(np.degrees(ry))
     transform.Update()
 
     transform_filter = vtk.vtkTransformFilter()
