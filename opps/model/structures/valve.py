c1bbd1edbb8ac0e0f5132dd7045b18e468e5098a
André Fernandes <fpf.andre@gmail.com>
2024-05-02 16:00:36 -0300

    Implement replace_point method for valves

--- opps/model/structures/valve.py
+++ opps/model/structures/valve.py
@@ -14,6 +14,13 @@ class Valve(Structure):
     def get_points(self) -> list[Point]:
         return [self.start, self.end]
 
+    def replace_point(self, old, new):
+        if self.start == old:
+            self.start = new
+
+        elif self.end == old:
+            self.end = new
+
     def as_dict(self) -> dict:
         return super().as_dict() | {
             "start": self.start,
