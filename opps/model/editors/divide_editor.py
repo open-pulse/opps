656cf882642937e5927b9705862982736d9f5ce8
André Fernandes <fpf.andre@gmail.com>
2024-05-17 17:31:52 -0300

    sketch divide editor

---
+++ opps/model/editors/divide_editor.py
@@ -0,0 +1,45 @@
+from .editor import Editor
+from opps.model import (
+    Bend, 
+    Elbow,
+    CBeam,
+    CircularBeam,
+    ExpansionJoint,
+    Flange,
+    IBeam,
+    Pipe,
+    RectangularBeam,
+    Reducer,
+    TBeam,
+    Valve,
+    Structure,
+    Beam,
+    Pipeline,
+    Point, 
+)
+
+
+class DivideEditor(Editor):
+    def divide_on_point(self, structure: Structure, point: Point):        
+        if isinstance(structure, Bend | Elbow):
+            pass  # Problem for future us
+
+        else: # Straight structures
+            new_structure = structure.copy()
+            structure.end = point
+            new_structure.start = point
+            self.pipeline.add_structure(new_structure)
+
+    def divide_evenly(self, strucutre: Structure, divisions: int):
+        pass
+
+    def interpolate_point(self, a: Point, b: Point, t: float) -> Point:
+        return a + t * (b - a)
+
+    def interpolate_evenly(self, a: Point, b: Point, divisions: int) -> list[Point]:
+        subdivisions = []
+        for i in range(divisions):
+            t = (i + 1) / (divisions + 1)
+            point = self.interpolate_point(a, b, t)
+            subdivisions.append(point)
+        return subdivisions
