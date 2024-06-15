fbe1804b52f4f1e08e6d593ce1b9b717303c2370
André Fernandes <fpf.andre@gmail.com>
2024-06-15 14:09:35 -0300

    reduce the resolution of pipes

--- opps/interface/viewer_3d/utils/cross_section_sources.py
+++ opps/interface/viewer_3d/utils/cross_section_sources.py
@@ -18,7 +18,7 @@ VALVE_WHEEL = load_symbol(SYMBOLS_DIR / "valve_wheel.obj")
 
 def closed_pipe_data(length, outside_diameter):
     cilinder = vtk.vtkCylinderSource()
-    cilinder.SetResolution(40)
+    cilinder.SetResolution(20)
     cilinder.SetRadius(outside_diameter / 2)
     cilinder.SetCenter(0, length / 2, 0)
     cilinder.SetHeight(length)
@@ -31,11 +31,12 @@ def pipe_data(length, outside_diameter, thickness):
     if (thickness == 0) or (2 * thickness > outside_diameter):
         return closed_pipe_data(length, outside_diameter)
 
+    sides = 20
     outer_radius = outside_diameter / 2
     inner_radius = (outside_diameter) / 2 - thickness
 
     outer_cilinder = vtk.vtkCylinderSource()
-    outer_cilinder.SetResolution(40)
+    outer_cilinder.SetResolution(sides)
     outer_cilinder.SetRadius(outer_radius)
     outer_cilinder.SetHeight(length)
     outer_cilinder.SetCenter(0, length / 2, 0)
@@ -43,7 +44,7 @@ def pipe_data(length, outside_diameter, thickness):
     outer_cilinder.Update()
 
     inner_cilinder = vtk.vtkCylinderSource()
-    inner_cilinder.SetResolution(40)
+    inner_cilinder.SetResolution(sides)
     inner_cilinder.SetRadius(inner_radius)
     inner_cilinder.SetHeight(length)
     inner_cilinder.SetCenter(0, length / 2, 0)
@@ -51,14 +52,14 @@ def pipe_data(length, outside_diameter, thickness):
     inner_cilinder.Update()
 
     ring_bottom = vtk.vtkDiskSource()
-    ring_bottom.SetCircumferentialResolution(40)
+    ring_bottom.SetCircumferentialResolution(sides)
     ring_bottom.SetOuterRadius(outer_radius)
     ring_bottom.SetInnerRadius(inner_radius)
     ring_bottom.SetNormal(0, 1, 0)
     ring_bottom.Update()
 
     ring_top = vtk.vtkDiskSource()
-    ring_top.SetCircumferentialResolution(40)
+    ring_top.SetCircumferentialResolution(sides)
     ring_top.SetOuterRadius(outer_radius)
     ring_top.SetInnerRadius(inner_radius)
     ring_top.SetCenter(0, length, 0)
